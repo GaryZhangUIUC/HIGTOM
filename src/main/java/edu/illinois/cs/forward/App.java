0a8afd873f0bd62692b6590239ba5da0ee2de0de
Yulun Du <yulundu2@illinois.edu>
2016-04-24 18:29:45 -0500

    add missing functions...

--- src/main/java/edu/illinois/cs/forward/App.java
+++ src/main/java/edu/illinois/cs/forward/App.java
@@ -9,11 +9,47 @@ import edu.illinois.cs.forward.types.Model;
 
 import java.util.HashMap;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.io.IOException;
+import java.util.*;
+import edu.illinois.cs.forward.types.Node;
 /**
  * The main program to run the framework
  *
  */
 public class App {
+    public static void printNode(Modeler m, Node n, Writer writer, int parent, int id, int level){
+
+        try {
+            writer.write(level + "");
+            writer.write("\t");
+            writer.write(id + "");
+            writer.write("\t");
+            writer.write(parent + "");
+            writer.write("\t");
+            writer.write(String.valueOf(n.location.latitude) + ", " + String.valueOf(n.location.longitude));
+            writer.write("\t");
+            for (Map.Entry<Integer,Integer> entry : n.wordCounts.entrySet()) {
+                Integer key = entry.getKey();
+                Integer value = entry.getValue();
+                writer.write(m.dataSet.id2Word.get(key));
+                writer.write(":");
+                writer.write(value + "");
+                writer.write(",");
+            }
+
+            writer.write("\n");
+        }
+        catch (IOException ex) {
+            // report
+            System.out.println( "error!" );
+        }
+
+    }
+
     public static void main( String[] args ) {
         TwitterJsonImporter importer = new TwitterJsonImporter(
                 "src/main/resources/chicago75000s.txt", "src/main/resources/stopwords.txt");
@@ -31,5 +67,53 @@ public class App {
         );
         modeler.estimate(10);
         System.out.println( "Good luck!" );
+
+
+
+                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("out.txt"), "utf-8"))) {
+
+
+            Node temp = modeler.root;
+
+            if( !temp.isLeaf()){
+
+                // root
+                printNode(modeler, temp, writer, 0, 0, 0);
+
+                for(int i = 0; i < temp.children.size(); i++){
+
+                    // 2nd level
+                    printNode(modeler, temp.children.get(i), writer, 0, i, 1);
+
+                    for(int j = 0; j < temp.children.get(i).children.size(); j++){
+
+                        // leafs
+                        printNode(modeler, temp.children.get(i).children.get(j), writer, i, j, 2);
+
+                    }
+                }
+
+
+            }
+
+
+
+
+        }
+        catch (IOException ex) {
+            // report
+            System.out.println( "file error!" );
+        }
+
+        
     }
+
+
+
+
+
+
+
+
 }
